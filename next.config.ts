ad9a637f8c9ab6169a28e15b5219bad88cf70872
AshuPatil04 <ashwinipatil8702@gmail.com>
2025-08-05 16:34:47 +0530

    Fix

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true, // ✅ Ignore TS build errors on Vercel
   },
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ Ignore ESLint errors on Vercel
-  },
 };
 
 export default nextConfig;
